4a3f9d3cdcdc123c76362c72873ca5ef4d0236a7
Haricr36 <126347853+Haricr36@users.noreply.github.com>
2023-03-22 20:21:23 +0530

    Add files via upload

---
+++ week5.day2/org/system/Computer.java
@@ -0,0 +1,11 @@
+package org.system;
+
+public class Computer {
+	
+	public void ComputerModel() {
+		
+		String ModelName  = "Apple MacBook Air";
+		System.out.println( "Computer Model : "+ ModelName);	
+
+}
+}
