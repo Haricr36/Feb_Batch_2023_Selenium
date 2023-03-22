9b9a368d2701139507a1481e6984caa2768002d4
Haricr36 <126347853+Haricr36@users.noreply.github.com>
2023-03-22 20:15:00 +0530

    Add files via upload

---
+++ org/college/package-info.java
@@ -0,0 +1,14 @@
+package org.college;
+/*
+Package   :org.college
+Class     :College
+Methods   :collegeName(),collegeCode(),collegeRank()
+
+Package   :org.department
+Class        :Department
+Methods   :deptName()
+
+Package   :org.student
+Class        :Student
+Methods   :studentName(),studentDept(),studentId()*/
+
---
+++ org/deparment/Deparment.java
@@ -0,0 +1,11 @@
+package org.deparment;
+
+import org.college.College;
+
+public class Deparment extends College {
+	public void DeparmentName() {
+		String dep = "B.E";
+		System.out.println("Dep name :" + dep );
+	}
+
+}
